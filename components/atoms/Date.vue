04c5cb0768ac40a205b74c07b5ccef14b5ed1953
vitormfrey <vitormoises.rey@gmail.com>
2021-04-30 01:54:20 -0300

    Criação do componente Date

---
+++ components/atoms/Date.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="container-date Date">
+    <h4>{{ meses[mes] }}</h4>
+    <p>{{ data }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      mes: 0,
+      data: '',
+      meses: [
+        'Janeiro',
+        'Fevereiro',
+        'Março',
+        'Abril',
+        'Maio',
+        'Junho',
+        'Julho',
+        'Agosto',
+        'Setembro',
+        'Outubro',
+        'Novembro',
+        'Dezembro'
+      ]
+    }
+  },
+  methods: {
+    date(): void {
+      const data = new Date()
+      const dia = data.getDate()
+      const mes = data.getMonth()
+      const ano = data.getFullYear()
+
+      const strData = dia + '/' + (mes + 1) + '/' + ano
+
+      // console.log(strData)
+      this.mes = mes
+      this.data = strData
+    }
+  },
+  created() {
+    this.date()
+  }
+})
+</script>
+
+<style scoped>
+.container-date {
+  display: grid;
+  justify-content: space-between;
+  grid-auto-flow: column;
+}
+h4,
+p {
+  font-size: 1.5rem;
+  color: #636363;
+}
+</style>
--- components/templates/HomeTemplate.vue
+++ components/templates/HomeTemplate.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    <HambuguerMenu />
-  </div>
+  <div><HomeBalance /></div>
 </template>
 
 <script lang="ts">

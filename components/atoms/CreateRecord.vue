16a353fc7f427a530371fecdaa1bdab8d3fa384b
vitormfrey <vitormoises.rey@gmail.com>
2021-05-03 20:57:38 -0300

    Criação componente CreateRecord e DataRealeases

---
+++ components/atoms/CreateRecord.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container-recordButton">
+    <NuxtLink to="/record" class="recordButton"
+      ><font-awesome-icon icon="plus" style="color: #fff" />
+    </NuxtLink>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style scoped>
+.container-recordButton {
+  display: grid;
+  justify-content: center;
+}
+.recordButton {
+  width: 60px;
+  height: 60px;
+  background-color: #126098;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 50%;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr;
+  justify-items: center;
+  align-items: center;
+}
+</style>
---
+++ components/molecules/DataRealeases.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="card">
+    <p class="category">{{ categoria }}</p>
+    <p class="description">Descrição: {{ name }}</p>
+    <div class="dot" :class="tipo ? 'expense' : 'revenue'"></div>
+    <WalletValue :valor="valor" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    categoria: { type: String, required: true },
+    name: { type: String, required: true },
+    valor: { type: Number, required: true },
+    tipo: { type: Boolean, required: true }
+  },
+  data() {
+    return {
+      revenue: 'revenue',
+      expense: 'expense'
+    }
+  },
+  computed: {}
+})
+</script>
+
+<style scoped>
+/* CARD */
+.card {
+  /* Grid */
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 25px);
+  grid-auto-flow: column;
+  padding: 1rem;
+  /* Estilização */
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0px 0px 6px -1px rgba(0, 0, 0, 0.25);
+}
+
+.category,
+.description {
+  justify-self: start;
+}
+.WalletValue,
+.dot {
+  justify-self: end;
+}
+
+/* Estilizações adicionais */
+.WalletValue {
+  font-size: 1.3rem;
+}
+.dot {
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+}
+.revenue {
+  background-color: green;
+}
+.expense {
+  background-color: red;
+}
+</style>

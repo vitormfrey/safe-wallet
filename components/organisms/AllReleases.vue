13613b95901ef55299f357667164b4608d10aab4
vitormfrey <vitormoises.rey@gmail.com>
2021-05-08 00:35:05 -0300

    Componente AllReleases criado

---
+++ components/organisms/AllReleases.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <ExpenseLaunches />
+    <RevenueLaunches />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style scoped>
+.container {
+  height: 100%;
+  display: grid;
+  grid-template-columns: 400px;
+  justify-content: center;
+  align-content: center;
+  grid-gap: 2.5rem;
+  margin-top: 40px;
+}
+</style>
